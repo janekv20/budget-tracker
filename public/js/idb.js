33029997a0014ceb1b787ec5f21ef65b8e38ab2d
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-21 21:57:10 -0500

    added to idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,22 @@
+//variable to hold db connection
+let db;
+//Connection to IndexDB database
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('transaction', {autoIncrememt: true });
+};
+
+request.onsucces = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction()
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
