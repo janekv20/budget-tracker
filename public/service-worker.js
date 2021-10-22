d45e04523a3096c714957372b4922d8ded3ed4ae
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-22 05:22:53 -0500

    fixed fetch function on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,4 +17,64 @@ const FILES_TO_CACHE = [
     './icons/icon-384x384.png',
     './icons/icon-512x512.png'
 
-]
+];
+//Service Worker installation
+self.addEventListener('install', function(e) {
+    e.waituntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('install cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+//Activation of Service Worker and deleting old cache data
+self.addEventListener('activate', function(e) {
+    e.waituntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Old cahced data being removed', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+//Fetch Requests
+self.addEventListener('fetch', function(e) {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(e.request)
+                .then(response => {
+                    if (response.status ===200) {
+                        cache.put(e.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(e.request);
+                })
+            })
+            .catch(err => console.log(err))
+        )
+        return;
+    }
+    e.respondWith(
+        fetch(e.request).catch(function () {
+            return caches.match(e.request).then(function (response) {
+                if (response) {
+                    return response;
+                }
+                else if (e.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            })
+        })
+    )
+})
